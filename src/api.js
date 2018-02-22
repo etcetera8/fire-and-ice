add5d9ee51f94f813a15b41e6ee088488698480f
parker lindley <fugazi8@gmail.com>
2018-02-22 13:00:17 -0700

    Adding sworn members jankily

--- src/api.js
+++ src/api.js
@@ -3,8 +3,17 @@ export const initialApiCall = async () => {
 
   const response = await fetch(`http://localhost:3001/api/v1/houses`);
   const resolvedResponse = await response.json();
-  const cleanedMembers = swornMemberCall(resolvedResponse)
-  return resolvedResponse;
+  const cleanedMembers = await swornMemberCall(resolvedResponse)
+ 
+  
+  const membersAdded = resolvedResponse.map((house, index) => {
+    house.swornMembers = cleanedMembers[index]
+    return house
+  })
+
+  console.log(membersAdded);
+
+  return membersAdded;
 } catch (error) {
   return "Error fetching data"
   }
@@ -19,7 +28,6 @@ export const swornMemberCall = async (arrayOfHouses) => {
     })
     return await Promise.all(unresolvedMembers)
   })
-  console.log("hi,",await Promise.all(unresolvedPromises));
   return Promise.all(unresolvedPromises)
 }
 
