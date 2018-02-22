e70310caa4d40d92c80664e8e3a2a057b312d9a9
parker lindley <fugazi8@gmail.com>
2018-02-22 12:48:32 -0700

    Getting all resolved members from call

--- src/api.js
+++ src/api.js
@@ -3,16 +3,32 @@ export const initialApiCall = async () => {
 
   const response = await fetch(`http://localhost:3001/api/v1/houses`);
   const resolvedResponse = await response.json();
+  const cleanedMembers = swornMemberCall(resolvedResponse)
   return resolvedResponse;
 } catch (error) {
   return "Error fetching data"
   }
 }
 
+export const swornMemberCall = async (arrayOfHouses) => {
+  const unresolvedPromises = arrayOfHouses.map( async (house) => {
+    const unresolvedMembers = house.swornMembers.map(async (url) => {
+      let memberFetch = await fetch(url)
+      let memberInfo = await memberFetch.json();
+      return memberInfo.name
+    })
+    return await Promise.all(unresolvedMembers)
+  })
+  console.log("hi,",await Promise.all(unresolvedPromises));
+  return Promise.all(unresolvedPromises)
+}
+
+//http://localhost:3001/api/v1/character/:id
+
 const initialCleaner = (array) => {
   const cleanedCards = array.map( card => {
     const {
-      name,
+      name, 
       ancestralWeapons,
       founded,
       seats,
