bf019d4962655dcd9bf291749a7ed6aac64c2a5f
parker lindley <fugazi8@gmail.com>
2018-02-22 13:39:22 -0700

    Have sworn members displaying conditionally

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,15 +22,9 @@ export class App extends Component {
     this.setState({loading: false})
   }
 
-  clicked = (house) => {
-    console.log('clicked', this, house)
-    
-  }
-
   makeCards = (array) => {
     const cardArray = array.map( (house, index) =>
       <Card
-        clicked={this.clicked}
         name={house.name} 
         coatOfArms={house.coatOfArms}
         founded={house.founded}
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css'
 
-export const Card = ({name, founded, seats, titles, coatOfArms, ancestralWeapons, words, swornMembers, clicked}) => {
+export class Card extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      clicked: false
+    }
+  }
+
+  clicked = () => {
+    console.log(this.state);
+    if (this.state.clicked === false) {
+      this.setState({clicked: true})
+    } else if (this.state.clicked === true) {
+      this.setState({clicked: false})
+      }
+    }
   
-  return (
-    <section className='Card' onClick={() => clicked(name)}>
-      <span className="house-info">{name}</span>
-      <span className="house-info">Founded: {founded}</span>
-      <span className="house-info">Seats: {seats}</span>
-      <span className="house-info">Titles: {titles}</span>
-      <span className="house-info">Coat of Arms: "{coatOfArms}"</span>
-      <span className="house-info">Ancestral Weapons: {ancestralWeapons}</span>
-      <span className="house-info">Words: {words}</span>
-    </section>
-  )
+  
+  render () {
+    const {name, founded, seats, titles, coatOfArms, ancestralWeapons, words, swornMembers} = this.props;
+    return (
+      <section className='Card' onClick={this.clicked}>
+        <span className="house-info">{name}</span>
+        <span className="house-info">Founded: {founded}</span>
+        <span className="house-info">Seats: {seats}</span>
+        <span className="house-info">Titles: {titles}</span>
+        <span className="house-info">Coat of Arms: "{coatOfArms}"</span>
+        <span className="house-info">Ancestral Weapons: {ancestralWeapons}</span>
+        <span className="house-info">Words: {words}</span>
+        {
+          this.state.clicked && 
+          <span>{swornMembers}</span>
+        }
+      </section>
+    )
+  }
 }
 
 
